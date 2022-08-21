feebb38f11c7424ba106844bb341c76f864e3183
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2022-08-21 11:09:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+choice = input("Choose betrween rock, paper, and scissors:")
+
+if choice ==      rock:
+   print("You have won")
+elif choice==    paper:
+     print("You didn't win")
+ elif choice==scissors:
+      print("You lost")
+ else:
+     print("Invalid choice, select either, rock, paper, or scissors")
